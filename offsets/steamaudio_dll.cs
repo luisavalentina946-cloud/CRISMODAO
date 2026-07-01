794a437c9bbbcfe74484c1ae3aa7e734576ac89c
Kernel User <kernel@loader.com>
2026-07-01 01:56:31 -0300

    Update via DUMPER UP GIT

--- offsets/steamaudio_dll.cs
+++ offsets/steamaudio_dll.cs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-06-10 22:44:23.452611400 UTC
+// 2026-07-01 04:54:47.033286300 UTC
 
 namespace CS2Dumper.Schemas {
     // Module: steamaudio.dll
