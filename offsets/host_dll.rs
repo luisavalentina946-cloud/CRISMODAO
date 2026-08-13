03ce5cd1f8c02af97f25883edf15b41efe81fd9d
Kernel User <kernel@loader.com>
2026-08-13 17:31:52 -0300

    Update via DUMPER UP GIT

--- offsets/host_dll.rs
+++ offsets/host_dll.rs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-08-11 21:59:51.233652600 UTC
+// 2026-08-13 20:31:35.747044800 UTC
 
 #![allow(non_upper_case_globals, non_camel_case_types, non_snake_case, unused)]
 
--- offsets/resourcesystem_dll.cs
+++ offsets/resourcesystem_dll.cs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2026-08-11 21:59:51.233652600 UTC
+// 2026-08-13 20:31:35.747044800 UTC
 
 namespace CS2Dumper.Schemas {
     // Module: resourcesystem.dll
